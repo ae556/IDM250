9b7f960210c8408bda27a6340b6765a28f430ff7
ae556 <72046954+ae556@users.noreply.github.com>
2023-02-20 22:48:37 -0500

    Add files via upload

---
+++ IDM250 themes/footer.php
@@ -0,0 +1,17 @@
+<?php
+// Link - https://developer.wordpress.org/reference/functions/wp_footer/
+wp_footer();
+?>
+
+<section class="footer">
+
+    <p>Teach Empower Advocate Learn</p>
+    <h4>-TEAL</h4>
+</section>
+
+
+
+
+</body>
+
+</html>
